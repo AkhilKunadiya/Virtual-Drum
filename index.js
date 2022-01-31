cf80a222a2b898d3c00e0c9bbd7b2d81bbce6aba
Akhil Kunadiya <akhil14d@gmail.com>
2022-01-31 19:13:15 +0530

    Add initial files

---
+++ index.js
@@ -0,0 +1,57 @@
+// Detecting Button Press
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  }); // what to do when click detected!
+}
+// Detecting keyboard press
+document.addEventListener("keypress", function(event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+          var tom1 = new Audio("Sounds/tom-1.mp3");
+          tom1.play();
+          break;
+        case "a":
+          var tom2 = new Audio("Sounds/tom-2.mp3");
+          tom2.play();
+          break;
+        case "s":
+          var tom3 = new Audio("Sounds/tom-3.mp3");
+          tom3.play();
+          break;
+        case "d":
+          var tom4 = new Audio("Sounds/tom-4.mp3");
+          tom4.play();
+          break;
+        case "j":
+          var snare = new Audio("Sounds/snare.mp3");
+          snare.play();
+          break;
+        case "k":
+          var crash = new Audio("Sounds/crash.mp3");
+          crash.play();
+          break;
+        case "l":
+          var kickBass = new Audio("Sounds/kick-bass.mp3");
+          kickBass.play();
+          break;
+  
+        default: console.log(buttonInnerHTML);
+      }
+}
+
+function buttonAnimation(currentKey) {
+        var activeButton = document.querySelector("." + currentKey);
+        activeButton.classList.add("pressed");
+        setTimeout(function() {
+            activeButton.classList.remove("pressed");
+        }, 100);
+
+}
